9a586557cb5517d701ff4be9e5d644245cc1efcd
Chris <chris.a.gonzalez97@gmail.com>
2023-07-05 13:37:16 -0700

    Modified repo to generate sn

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ def main():
     parser.add_argument('--params', type=str, help='Parameters JSON file.')
     parser.add_argument('--train', action='store_true', help='Flag to specify training.')
     parser.add_argument('--generate', action='store_true', help='Flag to specify dataset generation.')
+    parser.add_argument('--generate-sn', action='store_true', help='Flag to specify dataset generation from sensor network.')
     parser.add_argument('--test', action='store_true', help='Flag to specify testing.')
     parser.add_argument('--slsqp', action='store_true', help='Flag to specify using SLSQP baseline.')
     parser.add_argument('--trust-constr', action='store_true', help='Flag to specify using Trust Constraint baseline.')
@@ -47,6 +48,8 @@ def main():
         mcf_solver.train()
     elif args.generate:
         generate(params['generate'])
+    elif args.generate_sn:
+        generate_sn(params['generate_sn'])
     elif args.test:
         if args.slsqp:
             model_params['optimizer'] = {
@@ -89,7 +92,7 @@ def generate(params):
         is_address = params['is_address']
         save_graph(target=target, graph_name=graph_name, distance=distance, is_address=is_address)
 
-    graph = load_graph(graph_name=graph_name)
+    graph = load_grosmnxaph(graph_name=graph_name)
 
     dataset_name = params['dataset_name']
     dataset_folder = os.path.join('datasets', dataset_name)
@@ -147,6 +150,145 @@ def generate(params):
             if file_path == 'train':
                 source_index.add_item(i, source_demands)
                 sink_index.add_item(i, sink_demands)
+            else:
+                _, source_dist = source_index.get_nns_by_vector(source_demands, n=1, include_distances=True)
+                _, sink_dist = source_index.get_nns_by_vector(sink_demands, n=1, include_distances=True)
+
+                retries = 0
+                while source_dist[0] < demands_thresh and sink_dist[0] < demands_thresh:
+                    source_demands, sink_demands = create_demands(sources=sources, sinks=sinks)
+                    _, source_dist = source_index.get_nns_by_vector(source_demands, n=1, include_distances=True)
+                    _, sink_dist = source_index.get_nns_by_vector(sink_demands, n=1, include_distances=True)
+
+                    assert retries < 1000, 'Retried too many times.'
+                    retries += 1
+
+            dataset.append((source_demands, sink_demands))
+
+            # Periodically write to output files
+            if (i+1) % WRITE_THRESHOLD == 0:
+                index, _ = file_index(i)
+                write(dataset=dataset, folder=series_folder, index=index)
+                print('Completed {0}/{1} samples for {2}.'.format(i+1, num_samples, file_path))
+                dataset = []
+
+        if len(dataset) > 0:
+            index, _ = file_index(i)
+            write(dataset=dataset, folder=series_folder, index=index)
+        print('Completed {0}.'.format(file_path))
+
+        if file_path == 'train':
+            source_index.build(10)
+            sink_index.build(10)
+
+def generate_sn(params):
+
+    graph_name = params['graph_name']
+    graph_folder = os.path.join('graphs', graph_name)
+
+    # Fetch graph for the given query and save it if necessary
+    #if not os.path.exists(graph_folder):
+     #   target = params['osm_query']
+      #  distance = params.get('distance', 1000)
+       # is_address = params['is_address']
+        #save_graph(target=target, graph_name=graph_name, distance=distance, is_address=is_address)
+
+    #graph = load_graph(graph_name=graph_name)
+    #Read from .sn file to make nx graph
+
+    with open(params['sn_name'], 'r') as file:
+    # Read maximum x and y values for length and width (not used in this code snippet)
+        max_x, max_y, transmission_range = map(float, file.readline().split())
+
+        # Read capacity and packet count (not used in this code snippet)
+        capacity, packet_count = map(int, file.readline().split())
+
+        # Read number of nodes
+        num_nodes = int(file.readline())
+
+        # Create an empty graph
+        graph = nx.Graph()
+
+        # Iterate over the remaining lines in the file
+        for _ in range(num_nodes):
+            line = file.readline().split()
+            node_type = line[0]
+            x, y = map(float, line[1:3])
+
+            # Add the node to the graph
+            if node_type == 'd':
+                packet_value = int(line[3])
+                graph.add_node(node_type, x=x, y=y, packet_value=packet_value)
+            else:
+                graph.add_node(node_type, x=x, y=y)
+
+        # Compute distances between nodes and add edges
+        for u in graph.nodes():
+            for v in graph.nodes():
+                if u != v:
+                    distance = ((graph.nodes[u]['x'] - graph.nodes[v]['x'])**2 + (graph.nodes[u]['y'] - graph.nodes[v]['y'])**2) ** 0.5
+                    if distance <= transmission_range:
+                        graph.add_edge(u, v, weight=distance)
+
+
+    dataset_name = params['dataset_name']
+    dataset_folder = os.path.join('datasets', dataset_name)
+
+    if not os.path.exists(dataset_folder):
+        os.mkdir(dataset_folder)
+
+    # Save parameters
+    serialize_dict(dictionary=params, file_path=os.path.join(dataset_folder, 'params.pkl.gz'))
+
+    # Generate and save sources and sinks
+    if params['source_sink_strategy'] == 'random':
+        sources, sinks = random_sources_sinks(graph, num_sources=params['num_sources'], num_sinks=params['num_sinks'])
+    elif params['source_sink_strategy'] == 'farthest_sink':
+        sources, sinks = farthest_sink_nodes(graph, num_sources=params['num_sources'], num_sinks=params['num_sinks'])
+    else:
+        sources, sinks = farthest_nodes(graph, num_sources=params['num_sources'], num_sinks=params['num_sinks'])
+
+    G = graph.copy()
+    for source in sources:
+        G.add_node(source, demand=-packet_count)
+    for sink in sinks:
+        G.add_node(sink, demand=capacity)
+    file_path = os.path.join(dataset_folder, 'graph')
+    plot_road_flow_graph(graph=G, field='zero', graph_name=graph_name, file_path=file_path)
+
+    source_sink_dict = {
+        'sources': sources,
+        'sinks': sinks
+    }
+    serialize_dict(dictionary=source_sink_dict, file_path=os.path.join(dataset_folder, 'sources_sinks.pkl.gz'))
+
+    # Generate and save paths between sources and sinks
+    # paths = simple_paths(graph=graph, sources=sources, sinks=sinks, max_num_paths=params['max_num_paths'])
+    # serialize_dict(dictionary=paths, file_path=os.path.join(dataset_folder, 'paths.pkl.gz'))
+
+    demands_thresh = 0.1
+    source_index = AnnoyIndex(len(sources))
+    sink_index = AnnoyIndex(len(sinks))
+
+    file_paths = ['train', 'valid', 'test']
+    samples = [params['train_samples'], params['valid_samples'], params['test_samples']]
+    for file_path, num_samples in zip(file_paths, samples):
+
+        # Create folder to put this data series in
+        series_folder = os.path.join(dataset_folder, file_path)
+        if not os.path.exists(series_folder):
+            os.mkdir(series_folder)
+
+        dataset = []
+        for i in range(num_samples):
+            source_demands, sink_demands = create_demands(sources=sources, sinks=sinks)
+
+            # Ensure training demands not too close to either validation or test demands
+            if file_path == 'train':
+                source_index.add_item(i, source_demands)
+                sink_index.add_item(i, sink_demands)
+            #elif file_path=='test':
+                    #for now there is only one test sample, where the sources are the data nodes and the sinks are the storage nodes
             else:
                 _, source_dist = source_index.get_nns_by_vector(source_demands, n=1, include_distances=True)
                 _, sink_dist = source_index.get_nns_by_vector(sink_demands, n=1, include_distances=True)
